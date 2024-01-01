14963ddb1c0ffb8b25305dd804cf1cf161b617e8
FernDragonborn <FernDragonborn@gmail.com>
2024-01-01 22:46:07 +0200

    added account page, chaged login page LOgIn button

--- FrontendApp/src/app/components/drag-n-drop-upload/file-drag-n-drop.directive.ts
+++ FrontendApp/src/app/components/drag-n-drop-upload/file-drag-n-drop.directive.ts
@@ -8,35 +8,31 @@ export class FileDragNDropDirective {
   //@Input() private allowed_extensions : Array<string> = ['png', 'jpg', 'bmp'];
   @Output() private filesChangeEmiter : EventEmitter<File[]> = new EventEmitter();
   //@Output() private filesInvalidEmiter : EventEmitter<File[]> = new EventEmitter();
-  @HostBinding('style.background') private background = '#eee';
-  @HostBinding('style.border') private borderStyle = '2px dashed';
-  @HostBinding('style.border-color') private borderColor = '#696D7D';
-  @HostBinding('style.border-radius') private borderRadius = '5px';
+  @HostBinding('style.border') private borderStyle = '1px solid';
+  @HostBinding('style.border-color') private borderColor = '#121416';
+  @HostBinding('style.border-radius') private borderRadius = '0px';
 
   constructor() { }
 
   @HostListener('dragover', ['$event']) public onDragOver(evt){
     evt.preventDefault();
     evt.stopPropagation();
-    this.background = 'lightgray';
-    this.borderColor = 'cadetblue';
-    this.borderStyle = '3px solid';
+    this.borderColor = '#121416';
+    this.borderStyle = '1px solid';
   }
 
   @HostListener('dragleave', ['$event']) public onDragLeave(evt){
     evt.preventDefault();
     evt.stopPropagation();
-    this.background = '#eee';
-    this.borderColor = '#696D7D';
-    this.borderStyle = '2px dashed';
+    this.borderColor = '#121416';
+    this.borderStyle = '1px solid';
   }
 
   @HostListener('drop', ['$event']) public onDrop(evt){
     evt.preventDefault();
     evt.stopPropagation();
-    this.background = '#eee';
-    this.borderColor = '#696D7D';
-    this.borderStyle = '2px dashed';
+    this.borderColor = '#121416';
+    this.borderStyle = '1px solid';
     debugger;
     let files = evt.dataTransfer.files;
     let valid_files : Array<File> = files;
