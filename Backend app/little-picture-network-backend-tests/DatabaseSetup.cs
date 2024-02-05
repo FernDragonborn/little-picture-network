cd5d760d296d69726994b45862bf728300a60922
FernDragonborn <FernDragonborn@gmail.com>
2024-02-05 14:48:14 +0200

    added tests

---
+++ Backend app/little-picture-network-backend-tests/DatabaseSetup.cs
@@ -0,0 +1,19 @@
+﻿using LittlePictureNetworkBackend.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LittlePictureNetworkBackend_tests
+{
+    public class DatabaseSetup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Configure services for testing, e.g., using a Dockerized SQL Server
+            services.AddDbContext<PictureNetworkDbContext>(options =>
+                options.UseSqlServer("Server=localhost,1433;Database=YourDb;User=YourUser;Password=YourPassword"));
+
+            // Add other services needed for testing
+
+        }
+    }
+}
---
+++ Backend app/little-picture-network-backend-tests/Integration Tests/IntegrationTestWebAppFactory.cs
@@ -0,0 +1,29 @@
+﻿using LittlePictureNetworkBackend;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Data.Common;
+
+namespace LittlePictureNetworkBackend_tests.Integration_Tests
+{
+    public sealed class IntegrationTestWebAppFactory<TDbContext> : WebApplicationFactory<Program> where TDbContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public IntegrationTestWebAppFactory(IntegrationTests_AlbumController controllerFixture)
+        {
+            _connectionString = controllerFixture._msSqlContainer.GetConnectionString();
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.Remove(services.SingleOrDefault(service => typeof(DbContextOptions<TDbContext>) == service.ServiceType));
+                services.Remove(services.SingleOrDefault(service => typeof(DbConnection) == service.ServiceType));
+                services.AddDbContext<TDbContext>((_, option) => option.UseSqlServer(_connectionString));
+            });
+        }
+    }
+}
